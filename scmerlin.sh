0d95fd5c477157d901db535f1b321ad201e3dab1
Martinski <119833648+Martinski4GitHub@users.noreply.github.com>
2024-04-21 13:52:45 -0700

    Updated version string.

--- scmerlin.sh
+++ scmerlin.sh
@@ -11,7 +11,7 @@
 ##       https://github.com/jackyaz/scMerlin        ##
 ##                                                  ##
 ######################################################
-# Last Modified: 2024-Mar-12
+# Last Modified: 2024-Apr-21
 #-----------------------------------------------------
 
 ##########       Shellcheck directives     ###########
@@ -26,8 +26,8 @@
 ### Start of script variables ###
 readonly SCRIPT_NAME="scMerlin"
 readonly SCRIPT_NAME_LOWER="$(echo "$SCRIPT_NAME" | tr 'A-Z' 'a-z' | sed 's/d//')"
-readonly SCM_VERSION="v2.5.1"
-readonly SCRIPT_VERSION="v2.5.1"
+readonly SCM_VERSION="v2.5.2"
+readonly SCRIPT_VERSION="v2.5.2"
 SCRIPT_BRANCH="master"
 SCRIPT_REPO="https://raw.githubusercontent.com/decoderman/$SCRIPT_NAME/$SCRIPT_BRANCH"
 readonly SCRIPT_DIR="/jffs/addons/$SCRIPT_NAME_LOWER.d"
